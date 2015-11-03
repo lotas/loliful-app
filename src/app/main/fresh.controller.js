7735687fd5dfe75e97495f7797678fca90702f66
Yaraslau Kurmyza <me@yaraslav.com>
2015-11-03 19:31:38 +0100

    BE: FE: working on loading nails

--- src/app/main/fresh.controller.js
+++ src/app/main/fresh.controller.js
@@ -7,6 +7,11 @@ export class FreshController {
     constructor(User, Nail) {
         'ngInject';
 
+        this.User = User;
+        this.Nail = Nail;
     }
 
+    loadFresh() {
+        this.Nail.find()
+    }
 }
