5c5bbdf59f0e0d272eda34d98a0206b3d8872a63
Yaraslau Kurmyza <me@yaraslav.com>
2015-11-17 09:34:12 +0100

    FE: Changed useref version, fixed build paths for assets

--- gulp/build.js
+++ gulp/build.js
@@ -49,7 +49,7 @@ gulp.task('html', ['inject', 'partials'], function() {
         .pipe(jsFilter.restore)
         .pipe(cssFilter)
         .pipe($.sourcemaps.init())
-        .pipe($.replace('../../bower_components/bootstrap/fonts/', '../fonts/'))
+        .pipe($.replace('../../bower_components/bootstrap/fonts/', '../assets/fonts/'))
         .pipe($.minifyCss({processImport: false}))
         .pipe($.sourcemaps.write('maps'))
         .pipe(cssFilter.restore)
@@ -74,16 +74,17 @@ gulp.task('fonts', function() {
     return gulp.src($.mainBowerFiles())
         .pipe($.filter('**/*.{eot,svg,ttf,woff,woff2}'))
         .pipe($.flatten())
-        .pipe(gulp.dest(path.join(conf.paths.dist, '/fonts/')));
+        .pipe(gulp.dest(path.join(conf.paths.dist, '/assets/fonts/')));
+});
+
+var fileFilter = $.filter(function(file) {
+    return file.stat.isFile();
 });
 
 gulp.task('other', function() {
-    var fileFilter = $.filter(function(file) {
-        return file.stat.isFile();
-    });
-
     return gulp.src([
             path.join(conf.paths.src, '/**/*'),
+            path.join('!' + conf.paths.src, '/assets'),
             path.join('!' + conf.paths.src, '/config/*.json'),
             path.join('!' + conf.paths.src, '/**/*.{html,css,js,less,jade}')
         ])
@@ -91,8 +92,16 @@ gulp.task('other', function() {
         .pipe(gulp.dest(path.join(conf.paths.dist, '/')));
 });
 
+gulp.task('assets', function() {
+    return gulp.src([
+            path.join(conf.paths.src, '/assets/**/*')
+        ])
+        .pipe(fileFilter)
+        .pipe(gulp.dest(path.join(conf.paths.dist, '/assets')));
+});
+
 gulp.task('clean', function() {
     return $.del([path.join(conf.paths.dist, '/'), path.join(conf.paths.tmp, '/')]);
 });
 
-gulp.task('build', ['html', 'fonts', 'other']);
+gulp.task('build', ['html', 'fonts', 'other', 'assets']);
