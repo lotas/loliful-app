758a20d56758169b197b938998bcdaec99ccc5ee
Yaraslau Kurmyza <me@yaraslav.com>
2015-12-11 22:44:52 +0100

    FE: Added tests for Profile Controller

---
+++ src/app/user/profile/profile.controller.spec.js
@@ -0,0 +1,51 @@
+/* global beforeEach */
+/* global afterEach */
+/* global it */
+/* global describe */
+/* global jasmine */
+/* global expect */
+'use strict';
+
+describe('Profile controller', () => {
+    var $httpBackend;
+    var $rootScope;
+    var vm;
+
+    beforeEach(angular.mock.module('loliful'));
+
+    beforeEach(inject(($injector, $controller) => {
+        $httpBackend = $injector.get('$httpBackend');
+        $rootScope = $injector.get('$rootScope');
+
+        $httpBackend.expect('GET', 'http://local.loliful.io/users/stats').respond({stats: {}});
+        $httpBackend.expect('GET', 'http://local.loliful.io/me').respond({});
+
+        vm = $controller('ProfileController', {
+            currentUser: {
+                name: 'test',
+                id: 1
+            }
+        });
+    }));
+    afterEach(() => {
+        $httpBackend.verifyNoOutstandingExpectation();
+        $httpBackend.verifyNoOutstandingRequest();
+    });
+
+    it('Should load user stats', () => {
+        $httpBackend.flush();
+        $rootScope.$apply();
+        expect(vm.info).toEqual({});
+        expect(vm.stats).toEqual({});
+    });
+
+
+    it('should setName()', () => {
+        $httpBackend.flush();
+
+        $httpBackend.expect('POST', 'http://local.loliful.io/users/name').respond({name: 'name'});
+        vm.setName();
+        $httpBackend.flush();
+    });
+
+});
