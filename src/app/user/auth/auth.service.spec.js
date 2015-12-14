d174bd769f49d81a5e3609db14c3d3be85f03b2a
Yaraslau Kurmyza <me@yaraslav.com>
2015-12-14 20:44:55 +0100

    FE: Fixed frontend test for authorisation

--- src/app/user/auth/auth.service.spec.js
+++ src/app/user/auth/auth.service.spec.js
@@ -23,11 +23,26 @@ describe('Auth Service', () => {
         $httpBackend.flush();
     }));
 
+    it('Should call loadToken()', inject((AuthService) => {
+        $httpBackend.expect('GET', 'http://local.loliful.io/auth/token/1')
+                .respond({
+                    accessToken: {
+                        id: 1,
+                        userId: 2
+                    },
+                    user: 3
+                });
+        spyOn(AuthService, 'setToken').and.callThrough();
+        AuthService.loadToken(1);
+        $httpBackend.flush();
+        expect(AuthService.setToken).toHaveBeenCalledWith(1, 2, 3);
+    }));
+
     it('Should call setToken()', inject((AuthService, LoopBackAuth) => {
         spyOn(LoopBackAuth, 'setUser').and.returnValue(0);
         spyOn(LoopBackAuth, 'save').and.returnValue(1);
 
-        AuthService.setToken(1, 1);
+        AuthService.setToken(1, 1, 1);
         expect(LoopBackAuth.setUser).toHaveBeenCalledWith(1, 1, 1);
         expect(LoopBackAuth.save).toHaveBeenCalled();
     }));
