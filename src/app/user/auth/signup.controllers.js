ae9e606929bc521caa0b9d31ca9613994945c687
Yaraslau Kurmyza <me@yaraslav.com>
2016-02-07 16:53:45 +0100

    Let's fix unit-tests

--- src/app/user/auth/signup.controllers.js
+++ src/app/user/auth/signup.controllers.js
@@ -1,21 +1,9 @@
 export class SignupController {
     /**
      *
-     * @param {User} User
-     * @param {Storage} Storage
-     * @param {$window} $window
-     * @param {$log} $log
-     * @param toastr
-     * @param flags
      */
-    constructor(User, Storage, $window, $log, toastr, flags) {
-        'ngInject';
+    constructor() {
 
-        this.User = User;
-        this.Storage = Storage;
-        this.$log = $log;
-        this.$window = $window;
-        this.toastr = toastr;
     }
 
 }
