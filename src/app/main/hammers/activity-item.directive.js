beca9df9a4f4a0547d9687140bcf1d4090265d9a
Yaraslau Kurmyza <me@yaraslav.com>
2016-07-13 21:26:39 +0200

    Fixed vote&delete button in public activity page

--- src/app/main/hammers/activity-item.directive.js
+++ src/app/main/hammers/activity-item.directive.js
@@ -34,7 +34,6 @@ class HammerActivityItemController {
         this.isOwn = this.hammer.$user && String(AuthService.getUserId()) === String(this.hammer.$user.id);
     }
 
-    // TODO: copy-pasted functionality... yarik yarik
     delete() {
         if (this.isOwn) {
             this.SweetAlert.confirm('Remove', 'Is it this bad? Okay to remove it?', (res) => {
@@ -49,5 +48,30 @@ class HammerActivityItemController {
                 }
             });
         }
+    }
+
+    vote() {
+        if (this.isOwn) {
+            this.$log.warn('Oh why would you wanna do this..');
+            return false;
+        }
+
+        let method;
+        if (this.hammer.$votes) {
+            method = 'prototype$unvote';
+            this.hammer.$votes = false;
+        } else {
+            method = 'prototype$vote';
+            this.hammer.$votes = true;
+        }
+
+        this.Hammer[method]({
+            id: this.hammer.id
+        }).$promise.then(res => {
+            this.hammer.countVotes = res.countVotes;
+        }).catch(err => {
+            this.$log.debug(err);
+            this.toastr.warning('oh no!, vote failed');
+        });
     }
 }
